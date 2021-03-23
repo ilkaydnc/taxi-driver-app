859c15588e5a3a4ece42232ebfa69ac7b3f46dfa
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 18:21:33 +0300

    Fix import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import Layout from 'components/Layout'
+import Layout from '@components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
