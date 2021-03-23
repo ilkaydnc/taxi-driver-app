e6b9753e36e61d108d93d35370c884cb4d735efb
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 18:20:36 +0300

    Updat theme colors

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,34 +1,13 @@
 import React, { ReactNode } from 'react'
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-const palette = {
-  primary: {
-    light: '#35bdf2',
-    main: '#0442bf',
-    dark: '#056cf2',
-    contrastText: '#f2f2f2',
-  },
-  gray: {
-    50: '#F9FAFB',
-    100: '#F3F4F6',
-    200: '#E5E7EB',
-    300: '#D1D5DB',
-    400: '#9CA3AF',
-    500: '#6B7280',
-    600: '#4B5563',
-    700: '#374151',
-    800: '#1F2937',
-    900: '#111827',
-  },
+const colors = {
+  success: ['#5fd3d7', '#79e07c'],
+  danger: ['#515273', '#f53a68'],
 }
 
 const theme = {
-  palette,
-  border: {
-    light: palette.gray[50],
-    main: palette.gray[50],
-    dark: palette.gray[50],
-  },
+  colors,
 }
 
 export const GlobalStyle = createGlobalStyle`  
@@ -38,6 +17,8 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: #e4f1fa;
+    color: #2b2b2b;
   }
 
   a {
