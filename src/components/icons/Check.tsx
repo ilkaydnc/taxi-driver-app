3bd653ca41afaaa3919855d5d1c4302f0fe0d9a4
ilkaydnc <ilkaydinc1@gmail.com>
2021-03-23 22:17:31 +0300

    Create Check and Close icon components

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,19 @@
+const Check = () => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M5 13L9 17L19 7"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default Check
---
+++ src/components/icons/Close.tsx
@@ -0,0 +1,19 @@
+const Close = () => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6 6L18 18M6 18L18 6L6 18Z"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default Close
